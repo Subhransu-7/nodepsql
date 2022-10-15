58907609a6423434168605b146990da124eb649d
weldxit <113974251+weldxit@users.noreply.github.com>
2022-10-15 16:21:06 +0530

    Update pool.js

--- pool.js
+++ pool.js
@@ -1,9 +1,7 @@
-const { Pool } = require('pg')
-
-const pool= new Pool(
-    {
-       'postgres://subha:yigN6t5mtFNg1smmRb8L6OYGPeOBjGIJ@dpg-cd557gaen0hugpkke9bg-a/edb_efaz'
-    }
-)
+const { Pool} = require('pg')
+const connectionString = 'postgres://subha:yigN6t5mtFNg1smmRb8L6OYGPeOBjGIJ@dpg-cd557gaen0hugpkke9bg-a/edb_efaz'
+const pool = new Pool({
+  connectionString,
+})
 
 module.exports=pool
