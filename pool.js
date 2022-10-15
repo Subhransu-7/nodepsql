00418922e1be325664d9b1e94eb382512d6325af
weldxit <113974251+weldxit@users.noreply.github.com>
2022-10-15 12:16:02 +0530

    Update pool.js

--- pool.js
+++ pool.js
@@ -3,9 +3,9 @@ const { Pool } = require('pg')
 const pool= new Pool(
     {
         user:'subha',
-        host: 'localhost',
-        database:'api',
-        password:'subha123',
+        host: 'dpg-cd557gaen0hugpkke9bg-a',
+        database:'edb_efaz',
+        password:'yigN6t5mtFNg1smmRb8L6OYGPeOBjGIJ',
         port:5432,
     }
 )
