21a70108ab85ae0266a676a238cdab4435b0a15f
weldxit <113974251+weldxit@users.noreply.github.com>
2022-10-15 12:23:51 +0530

    Update pool.js

--- pool.js
+++ pool.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg')
 
 const pool= new Pool(
     {
-       postgres://subha:yigN6t5mtFNg1smmRb8L6OYGPeOBjGIJ@dpg-cd557gaen0hugpkke9bg-a/edb_efaz
+       'postgres://subha:yigN6t5mtFNg1smmRb8L6OYGPeOBjGIJ@dpg-cd557gaen0hugpkke9bg-a/edb_efaz'
     }
 )
 
