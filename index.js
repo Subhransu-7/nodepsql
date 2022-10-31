a8aa2020c03d46b2708088dbe4cc217cf71a7e8f
weldxit <113974251+weldxit@users.noreply.github.com>
2022-10-31 15:40:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const db= require('./queries')
+const cors=require('cors')
 
 const app=express();
-
+app.use(cors)
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true,}))
 
