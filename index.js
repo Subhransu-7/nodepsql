5f16fc912d65c7e893ac839716c87968c107f726
subha <subhransubidyasagar7@gmail.com>
2022-10-31 16:15:26 +0530

    updated index with cors

--- index.js
+++ index.js
@@ -4,21 +4,20 @@ const db= require('./queries')
 const cors=require('cors')
 
 const app=express();
-<<<<<<< HEAD
+
 const cors= require('cors')
 app.use(cors())
-=======
+
 app.use(cors)
->>>>>>> a8aa2020c03d46b2708088dbe4cc217cf71a7e8f
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true,}))
 
 app.listen(3000,()=>{console.log("server running on port 3000.....")})
 
-<<<<<<< HEAD
 
-=======
->>>>>>> a8aa2020c03d46b2708088dbe4cc217cf71a7e8f
+
+
 app.get('/users',db.getUsers),
 app.get('/products/:catagory',db.getProducts)
 
